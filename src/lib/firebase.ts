31c14faeeb74fc81d9c919cc3d2a4fe684445ed1
poligrup <poligrup44@gmail.com>
2025-09-15 20:54:50 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp, getApp, getApps } from 'firebase/app';
+
+const firebaseConfig = {
+  "projectId": "studio-5751693164-b0fd0",
+  "appId": "1:926945615988:web:5005143b865a423520f121",
+  "storageBucket": "studio-5751693164-b0fd0.firebasestorage.app",
+  "apiKey": "AIzaSyA0tJHXYfAfdX9bk0ao6AzFFd--QvMBFr4",
+  "authDomain": "studio-5751693164-b0fd0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "926945615988"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };
