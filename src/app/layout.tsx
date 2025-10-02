522e6470c02265b2cf6d16a68a5999a7b093ad90
poligrup <poligrup44@gmail.com>
2025-10-02 23:15:46 +0000

    I do not want to change that, I want to change the icon displayed before

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,9 @@ import { AuthProvider } from '@/hooks/use-auth';
 export const metadata: Metadata = {
   title: 'PoliSor',
   description: 'Akademik başarı için',
+  icons: {
+    icon: `data:image/svg+xml;base64,${btoa('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="hsl(24 95% 53%)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/></svg>')}`,
+  },
 };
 
 export default function RootLayout({
